f43d878d3418c479d61a68a13a333a7cabe099b6
jsz <jszpilewski@me.com>
2011-01-02 16:24:21 +0100

    eplists.sh

---
+++ eplists.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# reads in ~/.ep.py.rc and shows the next airing date for each show
+#
+# .ep.py.rc format:
+# 	showname
+# 	showname
+#   showname
+#
+# by d. arndt
+# https://github.com/daarndt
+#
+
+
+exec 10<~/.ep.py.rc
+
+while read LINE <&10; do
+	   ep.py "$LINE" | head -n 2
+done
+
+exec 10>&-
